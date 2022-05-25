eb97cd5dc6b2bbca9f8f7eb51f3391b3b76e0876
yubachiri <k.i.moso9532@gmail.com>
2022-05-25 20:19:50 +0900

    2019

--- numeron.rb
+++ numeron.rb
@@ -1,7 +1,7 @@
 # ## ゲームの流れ
 
-# - コンピューターの手をランダムに決める！
-# - プレイヤーが数字を入力する！
+# - ~~コンピューターの手をランダムに決める！~~
+# - ~~プレイヤーが数字を入力する！~~
 # - ジャッジしていく
 # - ジャッジ回数をカウントする
 
@@ -14,14 +14,18 @@ class Enemy
   def view_hand
     @hand
   end
+
+  def judge(predict)
+    # 桁と数字が合っていると1EAT
+    # 桁が異なるが、数字が合っていると1BITE
+    # @hand[0] predict[0]: EATは判定できるけどBITEが判定しづらい…
+    # BITE判定してから、位置が合っているものをEAT判定に変える
+  end
 end
 
 # ランダムな3桁の数字を生成するクラス
 class HandGenerator
   def self.generate
-    # 数字はみっつ 配列？
-    # 0~9の数字がある
-    # 重複なし
     (0..9).to_a.shuffle.slice(0..2)
   end
 end
