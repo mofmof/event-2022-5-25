1dd63bc7883be2944d08353351ab3b2804422a36
ShoKubota <sh2o.1114@mineo.jp>
2022-05-25 19:43:15 +0900

    19:43

---
+++ numeron.rb
@@ -0,0 +1,17 @@
+# ## ゲームの流れ
+
+# - コンピューターの手をランダムに決める！
+# - プレイヤーが数字を入力する！
+# - ジャッジしていく
+# - ジャッジ回数をカウントする
+
+class Enemy
+  def initialize
+    
+  end
+end
+
+class HandGenerator
+  def self.generate
+  end
+end
